26d9d3dc351106771590d2deab40f17d4c3eb464
Omkar Arora <aroraomkar@gmail.com>
2025-02-09 14:18:49 +0530

    feat: Added react hook form state management for sign-in page

--- screens/account/SignInPage.tsx
+++ screens/account/SignInPage.tsx
@@ -5,6 +5,7 @@ import { useThemeColor } from "@/hooks/useThemeColor";
 import { useSession } from "@/lib/SessionProvider";
 import { useRouter } from "expo-router";
 import {
+	Alert,
 	StyleSheet,
 	Text,
 	TextInput,
@@ -12,44 +13,98 @@ import {
 	View,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useForm, Controller } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+// Define the validation schema using Zod
+const schema = z.object({
+	username: z.string().min(1, "Username/Email is required"),
+	password: z.string().min(5, "Password must be at least 5 characters long"),
+});
+
+// Define the form data type
+type FormData = {
+	username: string;
+	password: string;
+};
 
 export default function SignInPage() {
 	const { signIn } = useSession();
 	const router = useRouter();
-	const primaryColor = useThemeColor({}, "primary");
+
+	const {
+		control,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<FormData>({
+		resolver: zodResolver(schema),
+	});
+
+	const onSubmit = (data: FormData) => {
+		console.log(data);
+
+		if (data.username === "admin" && data.password === "admin") {
+			signIn();
+			// Navigate after signing in. You may want to tweak this to ensure sign-in is
+			// successful before navigating.
+			router.replace("/(tabs)");
+		} else {
+			Alert.alert("Incorrect Email or Password");
+		}
+	};
+
 	return (
 		<SafeAreaView style={styles.container}>
-			<InputWrapper>
-				<IconSymbol name="person" color={"#111111"} />
-				<TextInput
-					placeholder="Email or Username"
-					style={styles.input}
-					autoComplete="email"
-					autoFocus
-				/>
-			</InputWrapper>
+			<Controller
+				control={control}
+				name="username"
+				render={({ field: { onChange, onBlur, value } }) => (
+					<InputWrapper>
+						<IconSymbol name="person" color={"#111111"} />
+						<TextInput
+							placeholder="Username or Email"
+							style={styles.input}
+							onBlur={onBlur}
+							onChangeText={onChange}
+							value={value}
+							autoComplete="email"
+							autoCapitalize="none"
+							autoFocus
+						/>
+					</InputWrapper>
+				)}
+			/>
 
-			<InputWrapper>
-				<IconSymbol name="lock" color={"#111111"} />
-				<TextInput
-					placeholder="Password"
-					autoComplete="password"
-					style={styles.input}
-					secureTextEntry
-				/>
-			</InputWrapper>
+			{errors.username && (
+				<Text style={styles.error}>{errors.username.message}</Text>
+			)}
+
+			<Controller
+				control={control}
+				name="password"
+				render={({ field: { onChange, onBlur, value } }) => (
+					<InputWrapper>
+						<IconSymbol name="lock" color={"#111111"} />
+						<TextInput
+							placeholder="Password"
+							autoComplete="password"
+							style={styles.input}
+							onBlur={onBlur}
+							onChangeText={onChange}
+							value={value}
+							secureTextEntry
+						/>
+					</InputWrapper>
+				)}
+			/>
+			{errors.password && (
+				<Text style={styles.error}>{errors.password.message}</Text>
+			)}
 
 			<Spacer width={"100%"} height={16} />
 
-			<PrimaryButton
-				onPress={() => {
-					signIn();
-					// Navigate after signing in. You may want to tweak this to ensure sign-in is
-					// successful before navigating.
-					router.replace("/(tabs)");
-				}}
-				title="Sign In"
-			/>
+			<PrimaryButton onPress={handleSubmit(onSubmit)} title="Sign In" />
 
 			<Spacer width={"100%"} height={8} />
 
@@ -84,5 +139,11 @@ const styles = StyleSheet.create({
 		flex: 1,
 		color: "#111111",
 		fontSize: 15,
+	},
+	error: {
+		color: "red",
+		marginBottom: 12,
+		textAlign: "left",
+		width: "100%",
 	},
 });
