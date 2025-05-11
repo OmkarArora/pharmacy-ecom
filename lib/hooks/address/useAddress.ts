69ba4b99f07515c13bf4f8e83530edd62795168a
Salil Palkar <salil.palkar99@gmail.com>
2025-05-11 13:22:24 +0530

    Added Address Get Call

--- lib/hooks/address/useAddress.ts
+++ lib/hooks/address/useAddress.ts
@@ -2,11 +2,11 @@ import { useStorageState } from "@/hooks/useStorageState";
 import { useSession } from "@/lib/SessionProvider";
 import { Address } from "@/lib/store/address-store";
 import { ADDRESS_BASE_URL } from "@/lib/values";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 export default function useAddress() {
-	// const [[_, username]] = useStorageState("username");
+	const [[_, username]] = useStorageState("username");
 	const { session } = useSession();
 
 	const addAddressMutation = useMutation({
@@ -28,5 +28,28 @@ export default function useAddress() {
 		},
 	});
 
-	return { addAddressMutation, deleteAddressMutation };
+	const addressQuery =  useQuery({
+		queryKey: ["get-address"],
+		queryFn: async () => {
+			try {
+				const response = await axios.get(
+					`${ADDRESS_BASE_URL}/address?user_name=${username}`,
+					{
+						headers: { Authorization: `${session}` },
+					}
+				);
+				console.log('response', response)
+				if (response.data) {
+					return response.data as Address[];
+				}
+				return [];
+			} catch (error) {
+				console.log("Error in fetching address", error);
+				return [];
+			}
+		},
+		enabled: !!(session && username)
+	});
+
+	return { addAddressMutation, deleteAddressMutation, addressQuery };
 }
--- screens/account/AddressBookPage.tsx
+++ screens/account/AddressBookPage.tsx
@@ -1,25 +1,40 @@
 import Header from "@/components/Header";
-import { Button, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from 'react';
+import { View, Text, Button, StyleSheet, FlatList, ActivityIndicator } from 'react-native';
 import { SafeAreaView } from "react-native-safe-area-context";
+import useAddress from "@/lib/hooks/address/useAddress";
+
 
 export default function AddressBookPage() {
+
+    const {addressQuery} = useAddress();
+	const {data, isLoading} = addressQuery;
+	console.log('data', data)
+	const renderAddress = ({ item } : any) => (
+		<View style={styles.addressItem}>
+			<Text style={styles.label}>
+							{item.line1}
+							{item.line2 ? `, ${item.line2}` : ''}
+							{`, ${item.city}, ${item.state} - ${item.pincode}`}
+			</Text>
+			<View style={styles.buttonRow}>
+				<Button title="Edit" onPress={() => alert(`Edit ${item.id}`)} />
+				<Button title="Delete" onPress={() => alert(`Delete ${item.id}`)} />
+			</View>
+		</View>
+	);
 	return (
 		<SafeAreaView style={{ flex: 1 }}>
 			<View style={{ padding: 16 }}>
 				<Header />
 				<View style={styles.addressContainer}>
 					<Text style={styles.heading}>Address Book</Text>
-					<View style={styles.addressItem}>
-						<Text style={styles.label}>123 Main St, Cityville</Text>
-						<View style={styles.buttonRow}>
-							<Button title="Edit" onPress={() => alert("Edit Address")} />
-							<Button title="Delete" onPress={() => alert("Delete Address")} />
-						</View>
-					</View>
-					<Button
-						title="Add New Address"
-						onPress={() => alert("Add Address")}
+					<FlatList
+						data={data}
+						keyExtractor={(item) => item.address_id.toString()}
+						renderItem={renderAddress}
 					/>
+					<Button title="Add New Address" onPress={() => alert('Add Address')} />
 				</View>
 			</View>
 		</SafeAreaView>
